75b603e1fdd5b15d97c2c3fd829341a5ff96caa0
Yash Wavhal <yashwavhal525@gmail.com>
2025-07-05 14:00:47 +0530

    frontend env

--- client/src/hooks/useFetch.ts
+++ client/src/hooks/useFetch.ts
@@ -6,11 +6,17 @@ function useFetch<T = any>(url: string) {
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<AxiosError | null>(null);
 
+  // Use environment variable or fallback
+  const BASE_URL =
+    process.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+
+  console.log("BASE_URL", BASE_URL);
+
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const res = await axios.get<T>(url);
+        const res = await axios.get<T>(BASE_URL + url);
         setData(res.data);
         setError(null);
       } catch (err) {
@@ -26,7 +32,7 @@ function useFetch<T = any>(url: string) {
   const reFetch = async () => {
     setLoading(true);
     try {
-      const res = await axios.get<T>(url);
+      const res = await axios.get<T>(BASE_URL + url);
       setData(res.data);
       setError(null);
     } catch (err) {
